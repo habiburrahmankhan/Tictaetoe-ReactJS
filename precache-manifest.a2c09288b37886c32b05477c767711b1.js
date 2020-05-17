ea6ccd93e3b9e9e8b2471c125d991bcf5662cea5
Habibur Rahman <habiburrahmankhan@Habiburrahman-khans-MacBook-Pro.local>
2020-05-17 05:57:22 +0530

    Updates

---
+++ precache-manifest.a2c09288b37886c32b05477c767711b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b930ea1d18478048f0d56a1faf386527",
+    "url": "/Tictaetoe-ReactJS/index.html"
+  },
+  {
+    "revision": "9cdb3fe21e97229714c0",
+    "url": "/Tictaetoe-ReactJS/static/css/main.fb515705.chunk.css"
+  },
+  {
+    "revision": "f62a4aa12d8162a59600",
+    "url": "/Tictaetoe-ReactJS/static/js/2.919d2037.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tictaetoe-ReactJS/static/js/2.919d2037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cdb3fe21e97229714c0",
+    "url": "/Tictaetoe-ReactJS/static/js/main.8172600f.chunk.js"
+  },
+  {
+    "revision": "cfb8ea4158e1ea9c36bb",
+    "url": "/Tictaetoe-ReactJS/static/js/runtime-main.331bb030.js"
+  }
+]);
